b6bf97ec39a77f7664b67beb258640d2215feca7
RickelviC <Cruzrickelvi@gmail.com>
2026-05-06 15:28:37 -0400

    player one has a hand

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Deck deck = new Deck();
+        Hand hand = new Hand();
+
+        deck.shuffle();
+        deck.shuffle();
+
+        System.out.print("Enter player 1 name: ");
+        String playerOne = scanner.next();
+
+        System.out.println(playerOne);
+        hand.deal(deck.deal());
+        hand.deal(deck.deal());
+
+        hand.print();
+
+    }
 }
